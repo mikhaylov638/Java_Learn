2bf7135d81d8bc4d750f861c6bfc8cc15f5adc14
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2022-07-05 19:32:25 +0300

    создан пакет для My_First_Program

--- Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/My_First_Program.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/My_First_Program.java
@@ -1,3 +1,5 @@
-public class My_First_Programm {
+package ru.stqa.sandbox.MyFirstProgram;
+
+public class My_First_Program {
     public static void main(String[] args) { System.out.println("Hello worlD");}
 }
