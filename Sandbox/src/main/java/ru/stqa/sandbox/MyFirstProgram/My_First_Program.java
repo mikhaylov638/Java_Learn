481b36397e61fdc403297a4c725c49760a2d66c9
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2022-07-05 19:56:37 +0300

    Переименовал класс в myFirstProgram

--- Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/My_First_Program.java
+++
@@ -1,5 +0,0 @@
-package ru.stqa.sandbox.MyFirstProgram;
-
-public class My_First_Program {
-    public static void main(String[] args) { System.out.println("Hello worlD");}
-}
---
+++ Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/myFirstProgram.java
@@ -0,0 +1,7 @@
+package ru.stqa.sandbox.MyFirstProgram;
+
+public class myFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello worlD");
+    }
+}
