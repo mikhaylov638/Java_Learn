b46661398c4c0e94371698cb453cacac59218265
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2022-07-06 17:47:52 +0300

    попробовал переменные

--- Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/myFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/myFirstProgram.java
@@ -2,6 +2,11 @@ package ru.stqa.sandbox.MyFirstProgram;
 
 public class myFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello worlD");
+        System.out.println("Hello" +  " worlD");
+
+
+        int l = 4;
+        int s = l*l;
+        System.out.println("square " + l + " = " + s);
     }
 }
