17fd1c8b54a08a549ce8a0da03460569bcd4bebc
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2022-07-07 18:01:27 +0300

    игрался с функциями. вроде выиграл

--- Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/myFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram/myFirstProgram.java
@@ -1,12 +1,37 @@
 package ru.stqa.sandbox.MyFirstProgram;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class myFirstProgram {
     public static void main(String[] args) {
-        String something = "World";
-        System.out.println("Hello " + something + "!");
+        String words = "abracadabra";
+        hello ("world");
+        hello (words);
+        hello ("3");
 
-        double l = 4.5;
-        double s = l*l;
-        System.out.println("square " + l + " = " + s);
+        System.out.println("");
+
+        double l = 2.5;
+        System.out.println("площадь квадрата со стороной " + l + " = " + square(l));
+
+        int a,b;
+        a = 5;
+        b = 2;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + rectangle(a, b));
+
     }
+
+    public static void hello (String something) {
+    System.out.println("Hello " + something + "!");
+    }
+    public static double square (double Leng){
+        return Leng * Leng;
+    }
+
+    public static int rectangle (int a, int b){
+        return a*b;
+    }
+
 }
